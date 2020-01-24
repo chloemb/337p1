746e56690016590c5a4f677a018836e1a69b4703
73Nixon73 <36314432+73Nixon73@users.noreply.github.com>
2020-01-23 18:18:13 -0600

    Tokenizing happens
    it tokenize

--- code.py
+++ code.py
@@ -1,10 +1,18 @@
 import nltk, numpy as np
 import json
+import sys
+
+nltk.download('averaged_perceptron_tagger')
 
 tweets=[]
 for line in open('gg2020.json','r', encoding='utf8'):
 	tweets.append(json.loads(line))
 
+propers=[]
+for line in tweets:
+	#parsed = nltk.tokenize(line['text'])
+	#print(parsed)
+	parsed = nltk.tag.pos_tag(line['text'].split())
+	print(type(parsed[0]))
 
-
-print(tweets[100])
+	sys.exit()
